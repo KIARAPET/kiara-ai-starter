6ada03be0ca1ff677c39a61765c876ce3c6c7fff
KIARAPET <lariarthurberbigier@gmail.com>
2025-11-04 19:55:04 -0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Atualiza pacotes e instala dependências necessárias para compilar asyncpg
+# Atualiza pacotes e instala dependências de compilação
 apt-get update && apt-get install -y build-essential python3-dev libpq-dev gcc
 
 # Atualiza pip e ferramentas
 pip install --upgrade pip setuptools wheel
 
-# Instala as dependências do projeto
+# Instala dependências do projeto
 pip install -r requirements.txt
